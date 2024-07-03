7d213a3d83d76410ca872254148ad4a9f149b311
AngelaGonzalezMarino <gonzalezmarinoangela@gmail.com>
2024-07-03 18:00:16 +0200

    update intel ips

--- corev_apu/fpga/intel/io_pll/io_pll_bb.v
+++ corev_apu/fpga/intel/io_pll/io_pll_bb.v
@@ -4,7 +4,9 @@ module io_pll (
 		input  wire  rst,      //   reset.reset
 		output wire  outclk_0, // outclk0.clk
 		output wire  outclk_1, // outclk1.clk
-		output wire  outclk_2  // outclk2.clk
+		output wire  outclk_2, // outclk2.clk
+		output wire  outclk_3, // outclk3.clk
+		output wire  outclk_4  // outclk4.clk
 	);
 endmodule
 
--- corev_apu/fpga/intel/io_pll/io_pll_inst.vhd
+++ corev_apu/fpga/intel/io_pll/io_pll_inst.vhd
@@ -5,7 +5,9 @@
 			rst      : in  std_logic := 'X'; -- reset
 			outclk_0 : out std_logic;        -- clk
 			outclk_1 : out std_logic;        -- clk
-			outclk_2 : out std_logic         -- clk
+			outclk_2 : out std_logic;        -- clk
+			outclk_3 : out std_logic;        -- clk
+			outclk_4 : out std_logic         -- clk
 		);
 	end component io_pll;
 
@@ -16,6 +18,8 @@
 			rst      => CONNECTED_TO_rst,      --   reset.reset
 			outclk_0 => CONNECTED_TO_outclk_0, -- outclk0.clk
 			outclk_1 => CONNECTED_TO_outclk_1, -- outclk1.clk
-			outclk_2 => CONNECTED_TO_outclk_2  -- outclk2.clk
+			outclk_2 => CONNECTED_TO_outclk_2, -- outclk2.clk
+			outclk_3 => CONNECTED_TO_outclk_3, -- outclk3.clk
+			outclk_4 => CONNECTED_TO_outclk_4  -- outclk4.clk
 		);
 
