612622cb88c22212092a2c8a19d97a9b1adce439
AngelaGonzalezMarino <gonzalezmarinoangela@gmail.com>
2024-10-16 11:22:12 +0200

    clean up fpga folder

--- corev_apu/fpga/pll_90_phase/pll_90_phase_bb.v
+++
@@ -1,9 +0,0 @@
-module pll_90_phase (
-		input  wire  refclk,     //     refclk.clk
-		output wire  locked,     //     locked.export
-		input  wire  rst,        //      reset.reset
-		input  wire  permit_cal, // permit_cal.export
-		output wire  outclk_0    //    outclk0.clk
-	);
-endmodule
-
