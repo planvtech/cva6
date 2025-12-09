79b674d5a6ff3fb0b40a4cb447affb01fd6fdffc
gonzalezmarinoangela <gonzalezmarinoangela@gmail.com>
2025-12-09 11:47:55 +0100

    extend smoke tests - axi configs fail

--- verif/regress/smoke-tests-cv32a60x_axi.sh
+++ verif/regress/smoke-tests-cv32a60x_axi.sh
@@ -47,6 +47,19 @@ CC_OPTS="-static \
 
 cd verif/sim/
 
+# 32-bit configurations without MMU
+riscv_tests_list=(
+  rv32ui-p-add
+  rv32ui-p-lw
+  rv32ui-p-sw
+  rv32ui-p-beq
+  rv32ui-p-jal
+)
+for t in ${riscv_tests_list[@]} ; do
+  python3 cva6.py --testlist=../tests/testlist_riscv-tests-cv32a60x-p.yaml --test $t --iss_yaml cva6.yaml --target cv32a60x_axi --iss=$DV_SIMULATORS $DV_OPTS
+  [[ $? > 0 ]] && ((errors++))
+done
+
 make -C ../.. clean
 make clean_all
 python3 cva6.py --iss_timeout=30000 --c_tests ../tests/custom/hello_world/hello_world.c --iss_yaml cva6.yaml --target cv32a60x_axi --iss=$DV_SIMULATORS --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld --gcc_opts="$CC_OPTS" $DV_OPTS --sv_seed 1
